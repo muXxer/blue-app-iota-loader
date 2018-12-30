1fdc4f54be11fff118cfccb188d7859c1268bda4
Wolfgang Welz <welzwo@gmail.com>
2018-12-30 23:22:11 +0100

    Update appFlags

--- download_app.py
+++ download_app.py
@@ -8,7 +8,7 @@ file_name           = "app.hex"
 data_size           = 0x00000040    # `cat debug/app.map |grep _nvram_data_size | tr -s ' ' | cut -f2 -d' '`
 icon_hex            = "0100000000ffffff00ffffffffffffffe7ffe75ffffff7e7ffe7fb7ffffffffff2fff3ffffffffffff"  # python $(BOLOS_SDK)/icon.py $(ICONNAME) hexbitmaponly
 cmd_check_version   = "-m get_ledger_version --targetId 0x%08X --minVersionOS 0x%08X --minVersionMCU 0x%08X" % (target_id, min_version_os, min_version_mcu)
-cmd_load_app        = "-m ledgerblue.loadApp --path \"44'/4218'\" --path \"44'/01'\" --appFlags 0x00 --tlv --targetId 0x%08X --delete --fileName %s --appName \"IOTA\" --appVersion 0.0.1 --dataSize 0x%08X --icon %s" % (target_id, file_name, data_size, icon_hex)
+cmd_load_app        = "-m ledgerblue.loadApp --path \"44'/4218'\" --path \"44'/01'\" --appFlags 0x40 --tlv --targetId 0x%08X --delete --fileName %s --appName \"IOTA\" --appVersion 0.0.1 --dataSize 0x%08X --icon %s" % (target_id, file_name, data_size, icon_hex)
 
 #===============================================================================
 if __name__ == '__main__':
